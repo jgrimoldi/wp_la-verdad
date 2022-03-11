4a07452c5c27c03621d326cbb7e2861709aaf5bc
jgrimoldi <jgrimoldi@frba.utn.edu.ar>
2022-03-11 20:01:00 -0300

    gitignore

--- admin/includes/admin_styles.php
+++ admin/includes/admin_styles.php
@@ -1,4 +1,4 @@
 <!-- ---- admin css ----  -->
 <link rel="stylesheet" href="<?php echo BASE_URL . 'admin/static/css/main.css' ?>" />
 <!-- ---- css theme ---- -->
-<link rel="alternate stylesheet" id="switcher-id" href="">
+<link rel="stylesheet" id="switcher-id" href="">
